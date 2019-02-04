4b33ab296a0d6323671fb0df57ec8788c1e63a67
SoribaD <soriba.dg@gmail.com>
2019-02-04 19:09:09 +0100

    adresse mail dans le footer

--- site/footer.php
+++ site/footer.php
@@ -5,7 +5,7 @@
 					<h2>Contactez-Nous</h2>
 					<ul class=\"actions\">
 						<li><span class=\"icon fa-phone\"></span> <a href=\"#\">(000) 000-0000</a></li>
-						<li><span class=\"icon fa-envelope\"></span> <a href=\"#\">blablasurf@gmail.com</a></li>
+						<li><span class=\"icon fa-envelope\"></span> <a href=\"#\">blablasurf2019@gmail.com</a></li>
 						<li><span class=\"icon fa-map-marker\"></span> 655 Avenue du Technopôle, Plouzané, 29280</li>
 					</ul>
 				</div>
